437e557427acbaf2628454956491d69b34655af0
Dmytro <dima.tumochko@gmail.com>
2023-02-26 12:06:54 +0200

    Add angular svg icon library with basic header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {SvgIconRegistryService} from "angular-svg-icon";
+import {Subscription} from "rxjs";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.less']
 })
-export class AppComponent {
-  title = 'etims-landing-ui';
+export class AppComponent implements OnInit, OnDestroy {
+
+    constructor(private iconReg: SvgIconRegistryService) {
+    }
+
+    public title = 'etims-landing-ui';
+    private iconRegSubscription: Subscription | undefined = new Subscription();
+
+    public ngOnInit(): void {
+        this.iconRegSubscription = this.iconReg.loadSvg("assets/icons/logo.svg", "logo")?.subscribe();
+    }
+
+    public ngOnDestroy(): void {
+        this.iconRegSubscription?.unsubscribe();
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {MatButtonModule} from "@angular/material/button";
+import {HeaderComponent} from './header/header.component';
+import {MatIconModule} from "@angular/material/icon";
+import {AngularSvgIconModule} from "angular-svg-icon";
+import {HomePageComponent} from './home-page/home-page.component';
+import {EtsFormCardComponent} from './shared/ets-form-card/ets-form-card.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        HomePageComponent,
+        EtsFormCardComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        MatSlideToggleModule,
+        MatButtonModule,
+        MatIconModule,
+        HttpClientModule,
+        AngularSvgIconModule.forRoot(),
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
