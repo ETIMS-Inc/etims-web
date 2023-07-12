3ccb3f9094d0540f5782e560159bcafa028ce0fe
Nikita <55248870+mechanoid7@users.noreply.github.com>
2023-07-12 21:53:00 +0300

    upd

---
+++ src/app/components/pages/courses-list-page/courses-list-page.component.ts
@@ -0,0 +1,36 @@
+import {Component} from "@angular/core";
+import {MenuItem} from "primeng/api";
+
+interface TempCourse { // TODO: use the real course model
+    id: string;
+}
+
+interface Tab {
+    id: string;
+    label: string;
+    courses?: TempCourse[];
+}
+
+@Component({
+    selector: "ets-courses-list-page",
+    templateUrl: "./courses-list-page.component.html",
+    styleUrls: ["./courses-list-page.component.less"],
+})
+export class CoursesListPageComponent {
+    public tabs: Tab[] = [
+        {id: "1", label: "All"},
+        {id: "2", label: "Favourite"},
+    ];
+    public activeTab: Tab = this.tabs[0];
+    public displayModes: any[] = [
+        {icon: "pi pi-list"},
+        {icon: "pi pi-th-large"},
+    ];
+
+    public currentMode: any = this.displayModes[0];
+    items: any;
+
+    onActiveItemChange($event: MenuItem) {
+
+    }
+}
---
+++ src/app/components/pages/courses-list-page/courses-list-page.module.ts
@@ -0,0 +1,26 @@
+import {CommonModule} from "@angular/common";
+import {NgModule} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+import {I18NextModule} from "angular-i18next";
+import {ButtonModule} from "primeng/button";
+import {SelectButtonModule} from "primeng/selectbutton";
+import {TabMenuModule} from "primeng/tabmenu";
+import {CoursesListPageRoutingModule} from "./courses-list-page-routing.module";
+import {CoursesListPageComponent} from "./courses-list-page.component";
+
+@NgModule({
+    declarations: [
+        CoursesListPageComponent,
+    ],
+    imports: [
+        CommonModule,
+        CoursesListPageRoutingModule,
+        I18NextModule,
+        ButtonModule,
+        SelectButtonModule,
+        FormsModule,
+        TabMenuModule,
+    ],
+})
+export class CoursesListPageModule {
+}
