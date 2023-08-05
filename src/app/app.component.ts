207c5100be5f54fc8215c4e621ec7203b7ae8fbe
Dmytro <dima.tumochko@gmail.com>
2023-08-05 17:04:01 +0300

    FE-26: add until-destroy library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,41 @@
 import {
     Component,
-    OnDestroy,
     OnInit,
 } from "@angular/core";
+import {
+    UntilDestroy,
+    untilDestroyed,
+} from "@ngneat/until-destroy";
 import {SvgIconRegistryService} from "angular-svg-icon";
-import {Subscription} from "rxjs";
 
+@UntilDestroy()
 @Component({
     selector: "app-root",
     templateUrl: "./app.component.html",
     styleUrls: ["./app.component.less"],
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent implements OnInit {
     public title = "etims-web";
-    private iconRegSubscription: Subscription | undefined = new Subscription();
 
     constructor(private iconReg: SvgIconRegistryService) {
     }
 
     public ngOnInit(): void {
-        this.iconReg.loadSvg("assets/icons/logo.svg", "logo")?.subscribe();
-        this.iconReg.loadSvg("assets/icons/global.svg", "global")?.subscribe();
-        this.iconReg.loadSvg("assets/icons/search.svg", "search")?.subscribe();
-        this.iconReg.loadSvg("assets/icons/dark-mode.svg", "dark-mode")?.subscribe();
-        this.iconReg.loadSvg("assets/icons/white-mode.svg", "white-mode")?.subscribe();
-        this.iconReg.loadSvg("assets/icons/flags/english.svg", "english")?.subscribe();
-        this.iconReg.loadSvg("assets/icons/flags/germany.svg", "germany")?.subscribe();
-        this.iconReg.loadSvg("assets/icons/flags/ukraine.svg", "ukraine")?.subscribe();
-    }
-
-    public ngOnDestroy(): void {
-        this.iconRegSubscription?.unsubscribe();
+        this.iconReg.loadSvg("assets/icons/logo.svg", "logo")
+            .pipe(untilDestroyed(this))?.subscribe();
+        this.iconReg.loadSvg("assets/icons/global.svg", "global")
+            .pipe(untilDestroyed(this))?.subscribe();
+        this.iconReg.loadSvg("assets/icons/search.svg", "search")
+            .pipe(untilDestroyed(this))?.subscribe();
+        this.iconReg.loadSvg("assets/icons/dark-mode.svg", "dark-mode")
+            .pipe(untilDestroyed(this))?.subscribe();
+        this.iconReg.loadSvg("assets/icons/white-mode.svg", "white-mode")
+            .pipe(untilDestroyed(this))?.subscribe();
+        this.iconReg.loadSvg("assets/icons/flags/english.svg", "english")
+            .pipe(untilDestroyed(this))?.subscribe();
+        this.iconReg.loadSvg("assets/icons/flags/germany.svg", "germany")
+            .pipe(untilDestroyed(this))?.subscribe();
+        this.iconReg.loadSvg("assets/icons/flags/ukraine.svg", "ukraine")
+            .pipe(untilDestroyed(this))?.subscribe();
     }
 }
