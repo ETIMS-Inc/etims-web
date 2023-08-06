e2c476970026f6f5f520cd75a088a6eb6e221fe9
Nikita <55248870+mechanoid7@users.noreply.github.com>
2023-08-06 22:07:25 +0300

    fix

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,21 +1,23 @@
 const gulp = require("gulp");
+const cheerio = require("gulp-cheerio");
+const es = require("event-stream");
 const fs = require("fs");
 const path = require("path");
+const replace = require("gulp-replace");
 const svgSprite = require("gulp-svg-sprites");
 const svgmin = require("gulp-svgmin");
-const cheerio = require("gulp-cheerio");
-const replace = require("gulp-replace");
-const gulpUtil = require('gulp-util');
 
 gulp.task("build-svgs", function () {
     const imageHolderFolder = "src/app/components/lib/icon/icon-holder";
     const imageHolderTemplateName = "icon-holder.component.html";
     const iconsPath = "src/assets/images/icons/*.svg";
+    const iconsColorfulPath = "src/assets/images/icons/colorful/*.svg";
 
     function updateIconsList() {
         const directoryPath = "src/assets/images/icons";
         const iconsListPath = "src/app/components/lib/icon/icons.list.ts";
 
+        // TODO: read from 2 places
         fs.readdir(directoryPath, function (err, files) {
             if (err) {
                 console.error("Icons list doesn't updated.")
@@ -35,6 +37,21 @@ gulp.task("build-svgs", function () {
                 console.log(`Icons list array was updated in "${iconsListPath}"`);
             });
         });
+    }
+
+    function getIcons(iconsPath, removeColors) {
+        if (removeColors) {
+            // remove redundant "fill", "style" props
+            return gulp.src(iconsPath)
+                .pipe(cheerio({
+                    run: function ($) {
+                        $("[fill]").removeAttr("fill");
+                        $("[style]").removeAttr("style");
+                    },
+                    parserOptions: {xmlMode: true},
+                }))
+        }
+        return gulp.src(iconsPath);
     }
 
     updateIconsList();
@@ -77,7 +94,7 @@ gulp.task("build-svgs", function () {
         "sortDefsChildren",
     ]; // to disable: .map(plugin => ({name: plugin, active: false}));
 
-    return gulp.src(iconsPath)
+    return es.merge(getIcons(iconsPath, true), getIcons(iconsColorfulPath))
         // minify svg
         .pipe(svgmin({
             multipass: true,
@@ -90,18 +107,8 @@ gulp.task("build-svgs", function () {
                 ...disabledPlugins,
             ],
         }))
-        // remove redundant(for us) prop
-        .pipe(cheerio({
-            run: function ($) {
-                if (gulpUtil.env.colorless) {
-                    $('[fill]').removeAttr('fill');
-                    $('[style]').removeAttr('style');
-                }
-            },
-            parserOptions: { xmlMode: true }
-        }))
         // cheerio plugin create unnecessary string '>', so replace it.
-        .pipe(replace('&gt;', '>'))
+        .pipe(replace("&gt;", ">"))
         // build svg sprite
         .pipe(svgSprite({
             mode: "symbols",
