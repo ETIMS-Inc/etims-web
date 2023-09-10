0feec2ee2c37e3f6d3bcb96b1588058c3747f89a
Nikita <55248870+mechanoid7@users.noreply.github.com>
2023-09-10 22:41:13 +0300

    upd

--- src/app/components/core-sidebar/sidebar-nav-item/sidebar-nav-item.component.ts
+++ src/app/components/core-sidebar/sidebar-nav-item/sidebar-nav-item.component.ts
@@ -34,19 +34,46 @@ import {
     ],
 })
 export class SidebarNavItemComponent {
-    @Input() public item: CoreSidebarNavItem;
-    @Input() public mode: CoreSidebarMode = CoreSidebarMode.Collapsed;
-    @Input() public currentUrl: string;
     @Output() public itemClicked: EventEmitter<CoreSidebarNavItem> = new EventEmitter();
+    public _item: CoreSidebarNavItem;
+    public _currentUrl: string;
+    public _mode: CoreSidebarMode = CoreSidebarMode.Collapsed;
     public sidebarMode = CoreSidebarMode;
     public displayNestedItems = false;
+    public nestedUrls: string[] = [];
+    public nestedItemSelected: boolean;
+
+    @Input()
+    set item(value: CoreSidebarNavItem) {
+        this._item = value;
+
+        if (value.childrenItems) {
+            this.nestedUrls = value.childrenItems.map(item => item.url);
+        }
+    }
+
+    @Input()
+    set currentUrl(value: string) {
+        this._currentUrl = value;
+
+        this.nestedItemSelected = this.nestedUrls.includes(value);
+    }
+
+    @Input()
+    set mode(value: CoreSidebarMode) {
+        this._mode = value;
+
+        if (this.nestedItemSelected) {
+            this.displayNestedItems = true;
+        }
+    }
 
     public toggleDisplayNestedItems() {
         this.displayNestedItems = !this.displayNestedItems;
     }
 
     public navItemClicked(navItem: CoreSidebarNavItem) {
-        if (this.mode === CoreSidebarMode.Collapsed) {
+        if (this._mode === CoreSidebarMode.Collapsed) {
             this.itemClicked.emit(navItem);
         } else {
             navItem.childrenItems ? this.toggleDisplayNestedItems() : this.itemClicked.emit(navItem);
