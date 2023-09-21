06c44b6fe21d6094995524424a2d71b05bb269f5
Nikita <55248870+mechanoid7@users.noreply.github.com>
2023-09-21 21:40:20 +0300

    upd

--- src/app/store/effects.ts
+++ src/app/store/effects.ts
@@ -1,10 +1,8 @@
 import {AuthEffects} from "./effects/auth.effects";
-import {DataEffects} from "./effects/data.effects";
 
 /**
  * Use alphabetical order for effects
  */
 export const etsEffects = [
     AuthEffects,
-    DataEffects,
 ];
