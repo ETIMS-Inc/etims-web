0989309246ea4225eba0f8dd1de8e8173975964e
Nikita <55248870+mechanoid7@users.noreply.github.com>
2023-10-16 23:53:47 +0300

    add nav-menu component

---
+++ src/app/components/lib/nav-menu/nav-menu.component.ts
@@ -0,0 +1,30 @@
+import {CommonModule} from "@angular/common";
+import {
+    ChangeDetectionStrategy,
+    Component,
+    Input,
+} from "@angular/core";
+import {NavItemComponent} from "../nav-item/nav-item.component";
+import {
+    NavItem,
+    NavItemDisplayMode,
+} from "../nav-item/nav-item.model";
+
+@Component({
+    selector: "ets-nav-menu",
+    standalone: true,
+    imports: [
+        CommonModule,
+        NavItemComponent,
+    ],
+    templateUrl: "./nav-menu.component.html",
+    styleUrls: ["./nav-menu.component.less"],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NavMenuComponent {
+    @Input() public items: NavItem[];
+
+
+    public navItemDisplayMode = NavItemDisplayMode;
+
+}
