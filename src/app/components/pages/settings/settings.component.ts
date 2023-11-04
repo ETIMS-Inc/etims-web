7b8e95a08808e11d07018d0907f6bde62b68336f
Nikita <55248870+mechanoid7@users.noreply.github.com>
2023-11-04 13:40:42 +0200

    upd settings page

--- src/app/components/pages/settings/settings.component.ts
+++ src/app/components/pages/settings/settings.component.ts
@@ -3,20 +3,17 @@ import {
     ChangeDetectionStrategy,
     Component,
 } from "@angular/core";
-import {Validators} from "@angular/forms";
+import {ActivatedRoute} from "@angular/router";
 import {ButtonModule} from "primeng/button";
 import {settingsNavMenuItems} from "../../../mocks/settings";
 import {CardComponent} from "../../lib/card/card.component";
-import {
-    FormFieldModel,
-    FormFieldType,
-} from "../../lib/form/form-field/form-field.model";
 import {FormRendererComponent} from "../../lib/form/form-renderer.component";
 import {FormRendererModel} from "../../lib/form/form-renderer.model";
 import {createFormGroupByFields} from "../../lib/form/form-renderer.utils";
 import {IconComponent} from "../../lib/icon/icon.component";
+import {NavItem} from "../../lib/nav-item/nav-item.model";
 import {NavMenuComponent} from "../../lib/nav-menu/nav-menu.component";
-import {profileFields} from "./settings.model";
+import {profileFields} from "./settings-fields.model";
 
 @Component({
     selector: "ets-settings",
@@ -34,10 +31,18 @@ import {profileFields} from "./settings.model";
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SettingsComponent {
-    public navMenuItems = settingsNavMenuItems;
+    public navMenuItems: NavItem[] = settingsNavMenuItems;
+
+    constructor(private activatedRoute: ActivatedRoute) {
+        console.log(">>> CONSTR: ", this.activatedRoute.url);
+    }
 
     public profileForm: FormRendererModel = {
         fields: profileFields,
         form: createFormGroupByFields(profileFields),
     };
+
+    public navChanged(navItem: NavItem) {
+        console.log(">>> navItem: ", navItem);
+    }
 }
--- src/app/components/pages/settings/settings.model.ts
+++ src/app/components/pages/settings/settings.model.ts
@@ -1,42 +1,13 @@
-import {Validators} from "@angular/forms";
-import {
-    FormFieldModel,
-    FormFieldType,
-} from "../../lib/form/form-field/form-field.model";
+import {NavItem} from "../../lib/nav-item/nav-item.model";
 
-export const profileFields: FormFieldModel[] = [
-    {
-        controlName: "firstName",
-        type: FormFieldType.STRING,
-        label: "First Name", // TODO: translate
-        required: true,
-        validators: [Validators.required],
-    },
-    {
-        controlName: "lastName",
-        type: FormFieldType.STRING,
-        label: "Last Name", // TODO: translate
-        required: true,
-        validators: [Validators.required],
-    },
-    {
-        controlName: "username",
-        type: FormFieldType.STRING,
-        label: "Username", // TODO: translate
-    },
-    {
-        controlName: "bio",
-        type: FormFieldType.TEXT_AREA,
-        label: "Bio", // TODO: translate
-    },
-    {
-        controlName: "email",
-        type: FormFieldType.EMAIL,
-        label: "Email", // TODO: translate
-        required: true,
-        validators: [
-            Validators.required,
-            Validators.email,
-        ],
-    },
-];
+// export enum SettingsTabs {
+//     Profile,
+//     Privacy,
+//     Security,
+//     Notifications,
+//     Integrations,
+// }
+//
+// export const SettingsNavTabs: Record<SettingsTabs, NavItem> = {
+//     [SettingsTabs.Profile]: {}
+// }
