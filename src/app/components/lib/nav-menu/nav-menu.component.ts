9fef11608386dbfca321b50060333c32a7c714e2
Nikita <55248870+mechanoid7@users.noreply.github.com>
2023-11-04 13:40:29 +0200

    Update nav-menu.component.ts

--- src/app/components/lib/nav-menu/nav-menu.component.ts
+++ src/app/components/lib/nav-menu/nav-menu.component.ts
@@ -2,8 +2,12 @@ import {CommonModule} from "@angular/common";
 import {
     ChangeDetectionStrategy,
     Component,
+    EventEmitter,
     Input,
+    OnInit,
+    Output,
 } from "@angular/core";
+import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
 import {
     NavigationEnd,
     Router,
@@ -33,23 +37,27 @@ import {
     styleUrls: ["./nav-menu.component.less"],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NavMenuComponent {
-    @Input() public items: NavItem[];
+export class NavMenuComponent implements OnInit {
+    @Input({required: true}) public items: NavItem[];
+    @Output() public navChanged: EventEmitter<NavItem> = new EventEmitter();
     public currentUrl: Observable<string>;
     public navItemDisplayMode = NavItemDisplayMode;
 
     constructor(private router: Router) {
         this.currentUrl = router.events.pipe(
+            takeUntilDestroyed(),
             filter(event => event instanceof NavigationEnd),
             map(event => (event as NavigationEnd).urlAfterRedirects),
             startWith(router.url),
         );
+    }
 
-        this.currentUrl.subscribe(val => console.log(">>> NAV: ", val));
-        // cdr.detectChanges();
+    public ngOnInit(): void {
+        this.navChanged.emit(this.items[0]);
     }
 
     public itemClicked(item: NavItem): void {
         this.router.navigateByUrl(item.url);
+        this.navChanged.emit(item);
     }
 }
