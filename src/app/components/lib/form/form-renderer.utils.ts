ed48f542a8ade241718833dc13d828dc9e3bfd3e
Nikita <55248870+mechanoid7@users.noreply.github.com>
2023-11-04 14:54:37 +0200

    Update form-renderer.utils.ts

--- src/app/components/lib/form/form-renderer.utils.ts
+++ src/app/components/lib/form/form-renderer.utils.ts
@@ -7,19 +7,20 @@ import {
     FormFieldModel,
     FormFieldType,
 } from "./form-field/form-field.model";
+import {FormRendererModel} from "./form-renderer.model";
 
 export function createFormGroupByFields(fields: FormFieldModel[]): FormGroup {
     const controls: Record<string, FormControl> = {};
     fields.forEach(field => {
         switch (field.type) {
             case FormFieldType.CHECKBOX:
-                controls[field.controlName] = new FormControl<boolean|null>(field.defaultValue as boolean, field.validators)
+                controls[field.controlName] = new FormControl<boolean | null>(field.defaultValue as boolean, field.validators);
                 break;
             case FormFieldType.DROPDOWN:
-                controls[field.controlName] = new FormControl<FormFieldDropdownOptionModel[]|null>(field.defaultValue as FormFieldDropdownOptionModel[], field.validators)
+                controls[field.controlName] = new FormControl<FormFieldDropdownOptionModel[] | null>(field.defaultValue as FormFieldDropdownOptionModel[], field.validators);
                 break;
             case FormFieldType.NUMBER:
-                controls[field.controlName] = new FormControl<number|null>(field.defaultValue as number, field.validators)
+                controls[field.controlName] = new FormControl<number | null>(field.defaultValue as number, field.validators);
                 break;
             case FormFieldType.EMAIL:
             case FormFieldType.PASSWORD:
@@ -27,9 +28,15 @@ export function createFormGroupByFields(fields: FormFieldModel[]): FormGroup {
             case FormFieldType.STRING:
             case FormFieldType.TEXT_AREA:
             default:
-                controls[field.controlName] = new FormControl<string|null>(field.defaultValue as string, field.validators)
+                controls[field.controlName] = new FormControl<string | null>(field.defaultValue as string, field.validators);
                 break;
         }
-    })
+    });
     return new FormGroup(controls);
 }
+
+export const getRendererForm = (fields: FormFieldModel[]): FormRendererModel =>
+    ({
+        fields: fields,
+        form: createFormGroupByFields(fields),
+    });
